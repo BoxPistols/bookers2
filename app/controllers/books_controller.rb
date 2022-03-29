68e53f81800aba7deee19a8332794a9d29294af3
Your Name <you@example.com>
2022-03-29 12:40:16 +0000

    タイトル・本文の投稿/TOPり一覧リダイレクト

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,31 @@
 class BooksController < ApplicationController
+  
   def index
+    @books = Book.all
   end
 
+  # 投稿データの作成
   def new
+    @book = Book.new
+  end
+
+  # 投稿データの保存
+  def create
+    @book = Book.new(book_params) # ストロングパラメーター
+    @book.save
+    redirect_to '/books'
   end
 
   def edit
   end
 
   def destroy
+  end
+
+  # ストロングパラメーター
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,23 @@ Rails.application.routes.draw do
 
   root to: 'homes#top'
 
-  get 'books/index'
-  get 'books/new'
-  get 'books/edit'
-  get 'books/show'
+  # get 'books/index'
+  get 'books' => 'books#index'
+
+  # new POST画面
+  get 'books/new' => 'books#new'
+  post 'books' => 'books#create'
+
+  # show 詳細画面
+  get 'books/:id' => 'books#show'
+  # get 'books/:id' => 'books#show', as: "book"
+
+  # edit 編集画面
+  # get 'books/:id/edit' => 'books#edit', as: "edit_book"
+  # patch 'books/:id' => 'books#update', as: 'update_book'
   
+  # destroy 削除
+  # delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
