92e1475397171017911f937f49ea30a69f25b6d3
Your Name <you@example.com>
2022-03-29 12:07:16 +0000

    Add Book Controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root to: 'homes#top'
+
+  get 'books/index'
+  get 'books/new'
+  get 'books/edit'
+  get 'books/show'
+  
 end
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_28_140553) do
+ActiveRecord::Schema.define(version: 2022_03_29_080848) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "body"
   end
 
   create_table "users", force: :cascade do |t|
